bc4535e21c60df5596a2766a7cb44355a82ddac2
noah candaele <58504453+noahCandaele@users.noreply.github.com>
2021-11-23 17:49:35 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-#!/usr/bin/python3
 import sys
 
-try:
-    name_file = sys.argv[1]
-    print(name_file)
-except :
-    print("Veuillez preciser le PDF")
+def getName():
+    try:
+        name_file = sys.argv[1]
+        print(name_file)
+    except :
+        print("Veuillez preciser le PDF")
+
+getName()
